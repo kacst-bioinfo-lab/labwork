5c40f64c9b4a2f3f6488f4cec4d1c67b31e6ec7e
AbuMohammad <atalgarni@gmail.com>
2017-02-04 07:19:32 +0300

    Add files via upload

--- code/abdulmalek/drosphila.py
+++ code/abdulmalek/drosphila.py
@@ -3,6 +3,7 @@
 
 from sys import argv
 
+
 InFile = open (argv[1], 'r')
 OutFile = open(argv[2], 'w')
 
@@ -15,9 +16,16 @@ for line in InFile:
       start = 0
       
       OutputColumn = ("{}\t{}\t{}\t{}\t".format(column[0],start,column[1],column[2]))
+      
       OutFile.write(OutputColumn + "\n")
       
       LineNumber = LineNumber + 1
+
       
 InFile.close()
-OutFile.close
+OutFile.close()
+
+
+import os
+os.system('cat chromInfo.bed | sort -k 1,1 -k2,2n| uniq > chromSort.bed')
+os.system('cat dm3_exons.txt | sort -k 1,1 -k2,2n| uniq > dm3_exonsSort.bed')
