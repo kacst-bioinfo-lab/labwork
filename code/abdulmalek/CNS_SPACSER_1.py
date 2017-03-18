471b444c1e7c8b7c0e255c3e98799f52fb14f770
AbuMohammad <atalgarni@gmail.com>
2017-03-18 13:58:44 -0800

    Update CNS_SPACSER_1.py

--- code/abdulmalek/CNS_SPACSER_1.py
+++ code/abdulmalek/CNS_SPACSER_1.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 def group(lst, n):# This is a generator function to read N items of a batch size n:
   for i in range(0, len(lst), n):
@@ -10,12 +9,11 @@ def group(lst, n):# This is a generator function to read N items of a batch size
 
 def Spacer_e(string):# This is a function to sum the uppercase letters for character in string:
     return sum(c.isupper() for c in string)
-
-   
+  
 def CNS_e(string):# This is a function to sum the lowercase letters for character in string:
     return sum(c.islower() for c in string)
        
-output_file = open('nucleotide_counts.bed','w') # the output file
+output_file = open('nucleotide_counts_2.bed','w') # the output file
 output_file.write('CA\tCB\tCR\tCB-CA\tCA-CR\tCB-CR\tSA\tSB\tSR\tSB-SA\tSA-SR\tSB-SR\n')#This is for the columns names
 
 
@@ -32,31 +30,28 @@ for r, row in enumerate(group(data,3)):
 
 for i, s in enumerate(group(all_seqs,3)):#group each three sequence in one batch of the size n & assigned it to batch s in i index
     A, B, R = s # This is to itertate inside each tupled batch & assign each item to its corresponding variable
-    As = (A[x-1:y]).lower()
-    Af = (A[y:z]).upper()
-    As2 = (A[z:]).lower()
-    Bs = (B[x-1:y]).lower()
-    Bf = (B[y:z]).upper()
-    Bs2 = (B[z:]).lower()
-    Rs = (R[x-1:y]).lower()
-    Rf = (R[y:z]).upper()
-    Rs2 = (R[z:]).lower()
-    CA = int(CNS_e(As) + CNS_e(As2))
-    CB = int(CNS_e(Bs) + CNS_e(Bs2))
-    CR = int(CNS_e(Rs) + CNS_e(Rs2))
-    CB_CA = int(CB-CA)
-    CA_CR = int(CA-CR)
-    CB_CR = int(CB-CR)
-    SA = int(Spacer_e(Af))
-    SB = int(Spacer_e(Bf))
-    SR = int(Spacer_e(Rf))
-    SB_SA = int(SB-SA)
-    SA_SR = int(SA-SR)
-    SB_SR = int(SB-SR)
+    Aslow = A[x:y]
+    Afast = A[y:z]
+    Aslow2 = A[z:]
+    Bslow = B[x:y]
+    Bfast = B[y:z]
+    Bslow2 = B[z:]
+    Rslow = R[x:y]
+    Rfast = R[y:z]
+    Rslow2 = R[z:]
+    CA = CNS_e(Aslow) + CNS_e(Aslow2) + CNS_e(Afast)# for line 29 to 40: just compute the sum of the spacer and CNS elements for each item
+    CB = CNS_e(Bslow) + CNS_e(Bslow2) + CNS_e(Bfast)
+    CR = CNS_e(Rslow) + CNS_e(Rslow2) + CNS_e(Rfast)
+    CB_CA = CB-CA
+    CA_CR = CA-CR
+    CB_CR = CB-CR
+    SA = Spacer_e(Afast) + Spacer_e(Aslow) + Spacer_e(Aslow2)
+    SB = Spacer_e(Bfast) + Spacer_e(Bslow) + Spacer_e(Bslow2)
+    SR = Spacer_e(Rfast) + Spacer_e(Rslow) + Spacer_e(Rslow2)
+    SB_SA = SB-SA
+    SA_SR = SA-SR
+    SB_SR = SB-SR
     #Printing the output into another file
     output_line = '%s\t%i\t%i\t%i\t%i\t%i\t%i\t%i\t%i\t%i\t%i\t%f\n' % \
-    (CA, CB, CR, CB_CA, CA_CR, CB_CR, SA, SB, SR, SB_SA, SA_SR, SB_SR)
+        (CA, CB, CR, CB_CA, CA_CR, CB_CR, SA, SB, SR, SB_SA, SA_SR, SB_SR)
     output_file.write(output_line)
-    if i is None:#Break condition I think I don't need it cuz it worked perfectley with out it.
-       break
-       output_file.close()
